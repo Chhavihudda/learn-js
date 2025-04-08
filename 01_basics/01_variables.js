6a71f1e010edf4c898ef0f820be8d76ca4ed083b
Chhavi Hudda <95969061+Chhavihudda@users.noreply.github.com>
2025-04-08 11:02:38 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,18 @@
+const accountId = 123654
+let accountEmail= "moni@gmail.com"
+var accountpassword= "123458"
+
+accountCity = "Ghaziabad"
+
+//const means fix the value
+
+// accountId=789 //not allowed
+
+accountEmail ="chhavi@gmail.com"
+accountpassword="789ASD"
+accountCity = "Delhi"
+
+let accountState ;
+console.log(accountId);
+
+console.table([accountId,accountEmail,accountpassword,accountCity,accountState])
